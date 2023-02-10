45da3b3abedd534d0e640ab5a285540e801a22df
justmogen <justmogen@gmail.com>
2023-02-10 06:35:21 +0000

    console

--- console.py
+++ console.py
@@ -12,6 +12,64 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = "(hbnb) "
 
+    def default(self, line):
+        """Catches commands that matches nothing"""
+        self._precmd(line)
+
+    def _precmd(self, line):
+        """intercepts commands to test for class.syntax()"""
+        match = re.search(r"^(\w*)\.(\w+)(?:\(([^)]*)\))$", line)
+        if not in match:
+            return line
+        classname = match.group(1)
+        method = match.group(2)
+        args = match.group(3)
+
+        match_uid = re.search('^"([^"]*)"(?:, (.*))?$', args)
+        if match_uid:
+            uid = match_uid.group(1)
+            dict_attr = match_uid.group(2)
+        else:
+            uid = args
+            dict_attr = False
+
+        attr_n_value = ""
+        if method == "update" and dict_attr:
+            match_dic = re.search('^({.*})$', dict_attr)
+            if matcch_dic:
+                self.update_dict(classname, uid, match_dic.group(1))
+                return ""
+            match_attr_n_value = re.search(
+                    '^(?:"([^"]*)")?(?:, (.*))?$', dict_attr)
+            if match_attr_n_value:
+                attr_n_value = (match_attr_n_value.group(1) or "") + " " +
+                (match_attr_n_value.group(2) or "")
+        cmd = method + " " + classname + " " + uid + " " + attr_n_value
+        self.onecmd(cmd)
+        return cmd
+
+    def update_dict(self, classname, uid, sr_dict):
+        """Helper method for update() with a dict.."""
+        sr = sr_dict.replace("'", '"')
+        dr = json.loads(sr)
+        if not classname:
+            print("** class name missing **")
+        elif classname not in storage.classes():
+            print("** class doesn't exist **")
+        elif uid is None:
+            print("** instance id missing **")
+        else:
+            key = "{}.{}".format(classname, uid)
+            if key not in storage.all():
+                print("** no instance found **")
+            else:
+                attributes = storage.attributes()[classname]
+                for attr, value in dr.items():
+                    if attributes in attributes:
+                        value = attributes[attributes](value)
+                        setattr(storage.all()[key], attribute, value)
+                storage.all()[key].save()
+
     def do_EOF(self, line):
         """Handles the End Of File character"""
         print()
@@ -38,7 +96,7 @@ class HBNBCommand(cmd.Cmd):
             print(obj.id)
 
     def do_show(self, line):
-        """Prints the string representation of an instance based 
+        """Prints the string representation of an instance based
         on the class name and id"""
         if line == "" or line is None:
             print("** class name missing **")
@@ -90,20 +148,19 @@ class HBNBCommand(cmd.Cmd):
             check_words = line.split(' ')
             if check_words[0] not in storage.classes():
                 print("** class doesn't exist **")
-            
+
             # creating nlist(newlist) of all the string repre..  of instances
             # of specified class by looping through all items in 'storage.all()
             # ' and checking if the type of each object matches the class name
             else:
-                nList = [str(obj) for key, obj in storage.all().items()
-                        if type(obj).__name__ == check_words[0]]
+                nList = [str(obj) for key, obj in storage.all().items() if
+                        type(obj).__name__ == check_words[0]]
                 print(nList)
 
         else:
             # no filtering of class name
             # all string repr of 'storage.all' of all instances are shown
             print([str(obj) for key, obj in storage.all().items()])
-
 
     def do_count(self, line):
         """counts the instances of the classes"""
@@ -116,9 +173,7 @@ class HBNBCommand(cmd.Cmd):
             matches = [
                 k for k in storage.all() if k.startswith(
                    check_words[0] + '.']
-            print(len(matches))
-
-
+                print(len(matches))
 
     def do_update(self, line):
         """Updates an instance based on the class name and id by adding or
@@ -129,12 +184,12 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return {}
 
-        reg = r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
-        match = re.search(reg, line)
-        classname = match.group(1)
-        uid = match.group(2)
-        attribute = match.group(3)
-        value = match.group(4)
+        reg=r'^(\S+)(?:\s(\S+)(?:\s(\S+)(?:\s((?:"[^"]*")|(?:(\S)+)))?)?)?'
+        match=re.search(reg, line)
+        classname=match.group(1)
+        uid=match.group(2)
+        attribute=match.group(3)
+        value=match.group(4)
 
         if not match:
             print("** class name missing **")
@@ -143,7 +198,7 @@ class HBNBCommand(cmd.Cmd):
         elif uid is None:
             print(" instance id is missing **")
         else:
-            key = "{}.{}".format(classname, uid)
+            key="{}.{}".format(classname, uid)
             if key not in storage.all():
                 print("** no instance found **")
             elif not attribute:
@@ -151,21 +206,21 @@ class HBNBCommand(cmd.Cmd):
             elif not value:
                 print("** value missing **")
             else:
-                push = None
+                push=None
                 if not re.search('^".*"$', value):
                     if '.' in value:
-                        push = float
+                        push=float
                     else:
-                        push = int
+                        push=int
                 else:
-                    value = value.replace('"', '')
+                    value=value.replace('"', '')
 
-                attributes = storage.attributes()[classname]
+                attributes=storage.attributes()[classname]
                 if attributes in attributes:
-                    value = attributes[attribute](value)
+                    value=attributes[attribute](value)
                 elif push:
                     try:
-                        value = push(value)
+                        value=push(value)
                     except ValueError:
                         pass
                     # The cast is done inside a try-except block, to catch any
@@ -176,8 +231,6 @@ class HBNBCommand(cmd.Cmd):
 
                 setattr(storage.all()[key], attribute, value)
                 storage.all()[key].save()
-
-
 
     if __name__ == '__main__':
         HBNBCommand().cmdloop()
